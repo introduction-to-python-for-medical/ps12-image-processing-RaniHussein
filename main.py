018b3d49901ba4ad14c2ace4093abebb29f0a34d
RaniHussein <ranihu46@gmail.com>
2025-01-27 00:07:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from image_utils import load_image, edge_detection
+from PIL import Image
+import numpy as np
+from skimage.filters import median
+from skimage.morphology import ball
+path = 'starry night.jpg'
+picture = load_image(path)
+clean_image = median(picture, ball(3))
+thefinal = edge_detection(clean_image)
+binary_image = thefinal < 100
 
+binary = Image.fromarray(binary_image)
+binary.save('mybinary.png')
